0997df8b2cb12cd4514bc2e7804f70085dcfd32c
Jiawei Yi <552388389@qq.com>
2024-05-10 16:17:42 +0000

    update

--- scripts/run_mag240_dist.sh
+++ scripts/run_mag240_dist.sh
@@ -1,4 +1,4 @@
-for i in 4 2 1
+for i in 4
 do
    python3 utils/launch_train.py --workspace /home/ubuntu/dgl_workspace/dgl-bench \
       --num_trainers 8 \
@@ -16,7 +16,43 @@ do
          --breakdown"
 done
 
-for i in 4 2 1
+for i in 4
+do
+   python3 utils/launch_train.py --workspace /home/ubuntu/dgl_workspace/dgl-bench \
+      --num_trainers 8 \
+      --num_samplers 1 \
+      --num_servers 1 \
+      --part_config /home/ubuntu/dgl_workspace/partition_dataset/mag240m_${i}p/mag240m.json \
+      --ip_config utils/ip_config${i}.txt \
+      "/home/ubuntu/miniconda3/envs/raft/bin/python3 training/train_dist_node_classification.py \
+         --graph_name mag240m \
+         --ip_config utils/ip_config${i}.txt \
+         --num_trainers 8 \
+         --num_epochs 4 --eval_every 999 \
+         --model gat --num_hidden 128 --num_heads 8 \
+         --fan_out 12,12,12 --batch_size 1536 \
+         --breakdown"
+done
+
+for i in 2 1
+do
+   python3 utils/launch_train.py --workspace /home/ubuntu/dgl_workspace/dgl-bench \
+      --num_trainers 8 \
+      --num_samplers 1 \
+      --num_servers 1 \
+      --part_config /home/ubuntu/dgl_workspace/partition_dataset/mag240m_${i}p/mag240m.json \
+      --ip_config utils/ip_config${i}.txt \
+      "/home/ubuntu/miniconda3/envs/raft/bin/python3 training/train_dist_node_classification.py \
+         --graph_name mag240m \
+         --ip_config utils/ip_config${i}.txt \
+         --num_trainers 8 \
+         --num_epochs 4 --eval_every 999 \
+         --model sage --num_hidden 128 --num_heads 8 \
+         --fan_out 12,12,12 --batch_size 1536 \
+         --breakdown"
+done
+
+for i in 2 1
 do
    python3 utils/launch_train.py --workspace /home/ubuntu/dgl_workspace/dgl-bench \
       --num_trainers 8 \
